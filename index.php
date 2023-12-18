ac7f5d2bf06163bd5b0b440fb4786a2f6bf27fb1
ApinopApinyachon <152172069+ApinopApinyachon@users.noreply.github.com>
2023-12-18 19:46:43 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,27 @@
 <html>
-    <p>
-        <h1>This is PHP <?php echo 'Hello World!';?></h1>
-    </p>
-
+<body>
+    <h1>This is PHP <?php echo 'Hello World!';?></h1>
     <div>
         <p>1</p>
         <?php
-        if(flase){
-            # die;
-            //
-            /*
-            */            
+        $var_a = 1;
+        $var_a = "1$var_a"; // 1 1
+        print_r($var_a);
+        echo "<br>";
+        #var_dump($var_a);
+        echo $var_a  .  "2"; // 12 , 3
+        $arry = array();
+        $arry = [];
+        $arry[] = 1;
+        $arry[] = 2;
+        $arry[0] = 3;
+        print_r($arry);
+        // count
+        // sizeof
+
+        for($i=0; $i < count($arry); $i++)  
+        {
+            echo "<h1>my h1</h1>";         
         ?>
         <p>2</p>
         <?php
@@ -20,7 +31,7 @@
         <p>3</p>
         
     </div>
-
+</body>
 
 
 </html>
