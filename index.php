008f5f00a31313cda02b686730a9c2f758f8dcd7
ApinopApinyachon <152172069+ApinopApinyachon@users.noreply.github.com>
2023-12-18 19:22:04 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,5 +2,25 @@
     <p>
         <h1>This is PHP <?php echo 'Hello World!';?></h1>
     </p>
+
+    <div>
+        <p>1</p>
+        <?php
+        if(flase){
+            # die;
+            //
+            /*
+            */            
+        ?>
+        <p>2</p>
+        <?php
+        }
+        ?>
     
+        <p>3</p>
+        
+    </div>
+
+
+
 </html>
