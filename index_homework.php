2283c74017ced74aef969d24dc7b91067acb3283
66160262 <66160262@go.buu.ac.th>
2023-12-21 17:34:11 +0700

    Create index_homework.php

---
+++ index_homework.php
@@ -0,0 +1,14 @@
+<html>
+
+    <head>
+        <meta charset="utf-8">
+    </head>
+
+    <body>
+        <h1><?php echo "Hello World<br><br>"; ?></h1>
+        <?php echo "<h1>Hello World<br><br></h1>" ?>
+        <?php $var_a = "SE CAMP" ?>
+        <?php echo $var_b = 'Hello  $var_a' ?>
+        <?php echo $var_c = "Hello $var_a" ?>
+    </body>
+</html>
