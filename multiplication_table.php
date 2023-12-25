f56175989a0201289d5d40b0f612f804f7c2b765
ApinopApinyachon <152172069+ApinopApinyachon@users.noreply.github.com>
2023-12-26 03:17:56 +0700

    Create multiplication_table.php

---
+++ multiplication_table.php
@@ -0,0 +1,54 @@
+<html>
+
+<head>
+    <meta charset="utf-8">
+</head>
+
+<body>
+    <?php $multi_x = 2; ?>
+    <?php $result = 0; ?>
+    <h1>ตารางสูตรคูณแม่ <?php echo $multi_x ?></h1>
+
+    
+    <div class=container>
+    <div>
+    
+            <?php 
+                for($i=1 ; $i <= 24 ; $i++){
+                    echo "<br/> $multi_x X $i = ".($multi_x * $i);
+                }
+            ?>
+        
+        </div>
+    </div>
+
+    
+    <style>
+        div{
+            
+            width: 150px;
+            height: 265px;
+            overflow-x: hide;
+            overflow-y: scroll;
+        }
+
+        h1{
+            
+            display: flex;
+            justify-content: center;
+        }
+
+        .container{
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            width: 100%;
+            margin: 0;
+            padding: 0;
+            overflow: auto;
+        }
+    </style>
+
+</body>
+
+</html>
